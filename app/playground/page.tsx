9c40b08476c252e81ba156de0c414432376d6014
Permanaa <irmanpermana.lu@gmail.com>
2024-09-01 12:42:04 +0700

    stagger on load & on click

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -8,8 +8,14 @@ export default function Playground() {
   const container = useRef<HTMLDivElement>(null)
 
   useGSAP(() => {
-    gsap.to(".box-load", {
-      rotation: 360
+    gsap.to(".box-load", { rotation: 360 })
+    gsap.to(".box-stagger", {
+      duration: 2,
+      rotation: 360,
+      opacity: 1,
+      stagger: 0.2,
+      ease: "sine.out", 
+      force3D: true
     })
   }, { scope: container })
 
@@ -19,13 +25,39 @@ export default function Playground() {
     gsap.to(".box-click", { rotation: "+=360" })
   })
 
+  const onClickX = contextSafe(() => {
+    gsap.to(".box-x", { x: 200, duration: 1 })
+  })
+
+  const onClickStagger = contextSafe(() => {
+    gsap.to(".box-stagger", {
+      duration: 0.5, 
+      opacity: 0, 
+      y: -100, 
+      stagger: 0.1,
+      ease: "back.in"
+    })
+  })
+
   return (
-    <main ref={container} className="flex min-h-screen items-start p-24 gap-4">
-      <div className="box-load bg-red-400 p-4 rounded text-white">
-        rotate on load
+    <main ref={container} className="flex min-h-screen items-start flex-col p-16 gap-4">
+      <div className="flex gap-4">
+        <div className="box-load bg-red-400 p-4 rounded text-white">
+          rotate on load
+        </div>
+        <div onClick={onClickRotate} className="box-click bg-blue-400 p-4 rounded text-white cursor-pointer">
+          rotate on click
+        </div>
+        <div onClick={onClickX} className="box-x bg-green-400 p-4 rounded text-white">
+          transform x on click
+        </div>
       </div>
-      <div onClick={onClickRotate} className="box-click bg-blue-400 p-4 rounded text-white cursor-pointer">
-        rotate on click
+      <div className="flex gap-4">
+        {new Array(5).fill("").map((_, index) => (
+          <div key={index} onClick={onClickStagger} className="box-stagger px-5 py-3 rounded bg-purple-400 text-white opacity-0">
+            {index}
+          </div>
+        ))}
       </div>
     </main>
   )
